e3c035107527d9ca18f685bd8e36188618f32dae
benjaminloboslorca <bloboslorca@outlook.com>
2025-04-01 08:45:12 -0300

    Agregar archivos de proyecto.

---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void mostrarMenu()
+    {
+        Console.WriteLine("=====Menu=====");
+        Console.WriteLine("1.Calculadora");
+        Console.WriteLine("2.Crear Nombre"); 
+    }
+    
+    static void crearNombre()
+    {
+        Console.WriteLine("Escriba su nombre: ");
+        string nombre = Console.ReadLine();
+        Console.WriteLine();
+    }
+    
+}
